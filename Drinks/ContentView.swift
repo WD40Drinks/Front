0b421b91a23852cee85e016f98f04b22f6a8d74c
Leonardo Rodrigues <leonardosrodrigues1@gmail.com>
2022-09-26 11:10:13 -0300

    Initial Commit

---
+++ Drinks/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Drinks
+//
+//  Created by Leonardo de Sousa Rodrigues on 26/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Drinks/DrinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinksApp.swift
+//  Drinks
+//
+//  Created by Leonardo de Sousa Rodrigues on 26/09/22.
+//
+
+import SwiftUI
+
+@main
+struct DrinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
