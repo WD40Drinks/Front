9ba1004051bebeeb56c585576fd2819cdeb30ff4
Gabriel Muelas <bimuelas@gmail.com>
2022-10-10 09:10:23 -0300

    Add colors and typography

---
+++ Drinks/Styles/Colors.swift
@@ -0,0 +1,24 @@
+//
+//  Colors.swift
+//  Drinks
+//
+//  Created by Gabriel Muelas on 10/10/22.
+//
+
+import SwiftUI
+
+extension Color {
+    enum App {
+        static let red = Color(red: 236, green: 101, blue: 100)
+        static let blue = Color(red: 60, green: 129, blue: 191)
+        static let green = Color(red: 152, green: 198, blue: 72)
+        static let purple = Color(red: 144, green: 124, blue: 182)
+        static let yellow = Color(red: 248, green: 202, blue: 88)
+        
+        static let lightRed = Color(red: 252, green: 232, blue: 232)
+        static let lightBlue = Color(red: 236, green: 243, blue: 249)
+        static let lightGreen = Color(red: 244, green: 249, blue: 235)
+        static let lightPurple = Color(red: 241, green: 239, blue: 246)
+        static let lightYellow = Color(red: 254, green: 247, blue: 230)
+    }
+}
