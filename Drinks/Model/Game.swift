b5ae22ed0123c2bd94888f3d6860dd1c2ccfe702
Leonardo Rodrigues <leonardosrodrigues1@gmail.com>
2022-10-10 08:50:48 -0300

    Add en and pt versions of mocked games and use only pt in app

--- Drinks/Model/Game.swift
+++ Drinks/Model/Game.swift
@@ -5,4 +5,7 @@ struct Game: Decodable {
     let text: String
     let imageName: String
     let rules: String?
+    let suggestions: [String]?
+    let timer: Int?
+    let minigameToken: String?
 }
--- Drinks/Model/GameFactory/MockGameFactory.swift
+++ Drinks/Model/GameFactory/MockGameFactory.swift
@@ -5,18 +5,7 @@ class MockGameFactory: GameFactory {
     private var games: [Game]
 
     required init() async throws {
-        guard let url = Bundle.main.url(forResource: "Games", withExtension: "json") else {
-            print("DEBUG: could not find Games.json")
-            throw MockGameFactoryError.mockFileNotFound
-        }
-
-        let data = try Data(contentsOf: url)
-        let games = try JSONDecoder().decode([Game].self, from: data)
-        if games.isEmpty {
-            throw GameFactoryError.emptyGameSource
-        }
-
-        self.games = games
+        self.games = try await Self.decodeGames()
     }
 
     func nextGame() throws -> Game {
@@ -30,6 +19,23 @@ class MockGameFactory: GameFactory {
         } else {
             nextGameIndex = 1
             return games[0]
+        }
+    }
+
+    private static func decodeGames() async throws -> [Game] {
+        guard let url = Bundle.main.url(forResource: "Games", withExtension: "json") else {
+            print("DEBUG: could not find Games.json")
+            throw MockGameFactoryError.mockFileNotFound
+        }
+
+        let data = try Data(contentsOf: url)
+        let games = try JSONDecoder().decode([[String: Game]].self, from: data)
+        if games.isEmpty {
+            throw GameFactoryError.emptyGameSource
+        }
+
+        return games.compactMap { languageGameMap in
+            languageGameMap["pt"]
         }
     }
 }
