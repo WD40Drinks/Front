5dc81d96a0dbc905b87991246e73cafbaf2f1bd9
Leonardo Rodrigues <leonardosrodrigues1@gmail.com>
2022-11-22 09:32:58 -0300

    Organize PromptView code

--- Drinks/Scenes/PromptView.swift
+++ Drinks/Scenes/PromptView.swift
@@ -2,31 +2,44 @@ import SwiftUI
 
 struct PromptView: UIViewRepresentable {
 
-    private let textView: PromptTextView
+    private let promptTextView: PromptTextView
     private let label: UILabel
     private let containerView: UIView
 
     init() {
-        self.textView = PromptTextView()
+        self.promptTextView = PromptTextView()
         self.label = UILabel()
         self.containerView = UIView()
-        label.text = NSLocalizedString("ready", comment: "")
-        label.textColor = .white
-        label.font = .App.promptTitle
     }
 
     func makeUIView(context: UIViewRepresentableContext<PromptView>) -> UIView {
         containerView.translatesAutoresizingMaskIntoConstraints = true
+        containerView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi / 2))
 
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        containerView.addSubview(textView)
+        configurePromptText()
+        configureLabel()
+
+        scheduleAnimations()
+
+        return containerView
+    }
+
+    private func configurePromptText() {
+        promptTextView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.addSubview(promptTextView)
 
         NSLayoutConstraint.activate([
-            textView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 8.0),
-            textView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -8.0),
-            textView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 64.0),
-            textView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -64.0)
+            promptTextView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 8.0),
+            promptTextView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -8.0),
+            promptTextView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 64.0),
+            promptTextView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -64.0)
         ])
+    }
+
+    private func configureLabel() {
+        label.text = NSLocalizedString("ready", comment: "")
+        label.textColor = .white
+        label.font = .App.promptTitle
 
         label.translatesAutoresizingMaskIntoConstraints = false
         containerView.addSubview(label)
@@ -35,9 +48,10 @@ struct PromptView: UIViewRepresentable {
             label.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
             label.centerYAnchor.constraint(equalTo: containerView.centerYAnchor)
         ])
+    }
 
-        containerView.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi / 2))
-        textView.alpha = 0
+    private func scheduleAnimations() {
+        promptTextView.alpha = 0
 
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
             UIView.animate(withDuration: 1.0) {
@@ -50,19 +64,16 @@ struct PromptView: UIViewRepresentable {
         DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
             UIView.animate(withDuration: 1.0) {
                 label.alpha = 0
-                textView.alpha = 1
+                promptTextView.alpha = 1
             }
         }
 
         DispatchQueue.main.asyncAfter(deadline: .now() + 4.0) {
-            textView.startScrolling()
+            promptTextView.startScrolling()
         }
-
-        return containerView
     }
 
     func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PromptView>) { }
-
 }
 
 private class PromptTextView: UITextView {
