81b2a1b00495c80cad4df079130f0b8eb0df9aee
Eduardo <eduardodiasdini@gmail.com>
2022-11-29 08:57:07 -0300

    First version of Timer

--- Drinks/Scenes/WhoAmIView.swift
+++ Drinks/Scenes/WhoAmIView.swift
@@ -6,20 +6,33 @@
 //
 
 import SwiftUI
+import Combine
 
 struct WhoAmI: View {
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
     var body: some View {
         HStack {
             Spacer()
             VStack {
                 Spacer()
-                Text("Hello")
-                Text("Hello")
-                Text("Hello")
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(Capsule())
+                Text("Fátima Bernardes")
                 Spacer()
             }
             Spacer()
+        }
+        .onReceive(timer) { _ in
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            }
         }
     }
 }
