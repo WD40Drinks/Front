0a69358b2667a20e81a3acdddf8f10fc44a56735
Eduardo <eduardodiasdini@gmail.com>
2022-11-29 09:09:01 -0300

    Two timers (one to start and another one to play)

--- Drinks/Scenes/WhoAmIView.swift
+++ Drinks/Scenes/WhoAmIView.swift
@@ -9,10 +9,45 @@ import SwiftUI
 import Combine
 
 struct WhoAmI: View {
+
+    enum WhoAmIStates {
+        case starting
+        case started
+    }
+
+    @State private var didStart = false
+    @State private var timeToStart = 5
     @State private var timeRemaining = 100
+    @State private var gameState: WhoAmIStates = .starting
+
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
     var body: some View {
+        switch gameState {
+        case .starting:
+            startingView
+        case .started:
+            startedView
+        }
+    }
+
+    private var startingView: some View {
+        VStack {
+            Spacer()
+            Text("Time: \(timeToStart)")
+            Spacer()
+        }
+        .onReceive(timer) { _ in
+            if !didStart && timeToStart > 0 {
+                timeToStart -= 1
+            } else if timeToStart == 0 {
+                didStart = true
+                gameState = .started
+            }
+        }
+    }
+
+    private var startedView: some View {
         HStack {
             Spacer()
             VStack {
@@ -25,6 +60,10 @@ struct WhoAmI: View {
                     .background(.black.opacity(0.75))
                     .clipShape(Capsule())
                 Text("Fátima Bernardes")
+                    .font(.App.title)
+                    .multilineTextAlignment(.center)
+                    .frame(minHeight: 200)
+                    .padding(.horizontal, 32)
                 Spacer()
             }
             Spacer()
